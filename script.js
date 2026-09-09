be13b9c64a1a15ed69db1ed28e8279909b79844d
anacarolina-wq <anacarolinacretellasouzamesadr@gmail.com>
2026-09-09 11:47:42 -0300

    ok

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document,querySelector(".post-media");
+    if (!likeBtn) return;
+
+    const likesCounrSpan = likeBtn.querySelector(" .likes-count");
+    const bookmarkBtn = document.querySelector(". bookmark-btn");
+
+    let isLiked = false;
+    let baseLikes = 0;
+
+    //testo inicial
+
+    if(likesCountSpan){
+        likesCountSpan.textContent = "0"; 
+    }
+
+    //numeros acima de 1mil
+    function formatLikes (num){
+        if (num >=1000){
+            return (num/1000).toFixed(1)+"K";
+        }
+        retorn num.toString();
+    }
+
+
+
 })
